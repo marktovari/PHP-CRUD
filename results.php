7e41dc9887ea189e0e4dbd125fa24b1935ae0f3b
Mark Tovari <mark.tovari@weboldo.com>
2022-10-27 11:59:00 +0200

    Added comments to DB opening, and moved freeing memory to footer

--- results.php
+++ results.php
@@ -2,7 +2,8 @@
 
 	//DB connect
 	define('__ROOT__', dirname(__FILE__));
-	require_once(__ROOT__.'\dbConnect.php'); //This is a file outside of the webroot to keep the info secure, don't forget to change it
+	//define('__ROOT__', dirname(dirname(__FILE__))); This is a file outside of the webroot with sensitive info to keep the info secure,
+	require_once(__ROOT__.'\dbConnect.php'); //Delete this and use the line above in actual application
 
 	//Write Queries in SQL and put it in a string
 	$queryParmeter =  "SELECT * FROM `testtable`;";
@@ -10,10 +11,6 @@
 	$queryResult = mysqli_query($connection, $queryParmeter);
 	//Fetch the resulting columns as an associative array
 	$queryArray = mysqli_fetch_all($queryResult, MYSQLI_ASSOC);
-
-	// Clean data from memory for better performance, then close off the connection to the Database
-	mysqli_free_result($queryResult);
-	mysqli_close($connection);
 
 ?>
 
--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,10 @@
 	<footer>
 		<div>&copy; Copyright Date Name</div>
 	</footer>
+
+		<?php
+		// Clean data from memory for better performance, then close off the connection to the Database
+		mysqli_free_result($queryResult);
+		mysqli_close($connection);
+	?>
 </body>
